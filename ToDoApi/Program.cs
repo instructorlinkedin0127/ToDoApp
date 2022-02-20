b6c41806666e2c5a85468c5b011dde5b20022390
Azure Instructor <instructor.azure06@red30tech.com>
2022-02-21 00:37:15 +0100

    Erreur de compilation.

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -24,7 +24,7 @@ if (app.Environment.IsDevelopment())
 
 app.MapFallback(() => Results.Redirect("/swagger"));
 
-// Get Task by Id
+// Find a task by Id
 app.MapGet("/todos/{id}", async (ToDoApiDbContext db, int id) =>
 {
     return await db.Todos.FindAsync(id) switch
@@ -34,7 +34,6 @@ app.MapGet("/todos/{id}", async (ToDoApiDbContext db, int id) =>
     };
 });
 
-// Get Tasks
 app.MapGet("/todos", async (ToDoApiDbContext db) =>
 {
     return await db.Todos.ToListAsync();
