e77dcd15985d963994670ca88dcb9e5d852297cc
Azure Instructor <instructor.azure06@red30tech.com>
2022-02-21 23:24:26 +0100

    Mise à joure de la base de donnée

--- ToDoApi/Migrations/20220221222334_AjoutColonnes.Designer.cs
+++ ToDoApi/Migrations/20220221222334_AjoutColonnes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ni_Soft.ToDoApi.Data;
 
@@ -10,9 +11,10 @@ using Ni_Soft.ToDoApi.Data;
 namespace Ni_Soft.ToDoApi.Migrations
 {
     [DbContext(typeof(ToDoApiDbContext))]
-    partial class ToDoApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220221222334_AjoutColonnes")]
+    partial class AjoutColonnes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.2");
